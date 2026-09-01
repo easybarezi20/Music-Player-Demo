d9628c8e115f4dacd9debe664a38511fb3c9e96a
easybarezi20@gmail.com <easybarezi20@gmail.com>
2026-09-01 16:27:32 -0700

    redo constructors made

--- Project2/Song.h
+++ Project2/Song.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef SONG_H
+#define SONG_H
+
+#include <string>
+
+class Node 
+{
+public:
+	// constructors
+	Node() : name("Blank"), artist("Blank"), next(nullptr) {}
+	Node(std::string newName) : name(newName) , artist("Blank") , next(nullptr) {}
+	Node(std::string newName, std::string newArtist) : name(newName) , artist(newArtist) , next(nullptr) {}
+	Node(std::string newName, std::string newArtist, Node* newNext) : name(newName), artist(newArtist), next(newNext) {}
+	//setters
+
+	//getters
+
+
+private:
+	std::string name;
+	std::string artist;
+	Node* next;
+};
+
+#endif 
