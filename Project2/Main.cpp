1c09306aace1f3a5ae3da64f590852d5622c0906
easybarezi20@gmail.com <easybarezi20@gmail.com>
2026-09-01 15:58:42 -0700

    Add project files.

---
+++ Project2/Main.cpp
@@ -0,0 +1,20 @@
+/*
+	Barezi Morales
+
+	9/1/2026
+
+	linked list music player Demo
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	cout << endl; 
+	system("Pause");
+	return 0;
+}
