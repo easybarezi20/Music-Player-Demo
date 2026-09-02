594231cfd1d800b1d91f0c071603467b2a288dde
easybarezi20@gmail.com <easybarezi20@gmail.com>
2026-09-01 17:32:51 -0700

    Song.h file fully complete

--- Project2/Song.h
+++ Project2/Song.h
@@ -12,9 +12,13 @@ public:
 	Node(std::string newName, std::string newArtist) : name(newName) , artist(newArtist) , next(nullptr) {}
 	Node(std::string newName, std::string newArtist, Node* newNext) : name(newName), artist(newArtist), next(newNext) {}
 	//setters
-
+	void setName(std::string newName) { name = newName; }
+	void setArtist(std::string newArtist) { artist = newArtist; }
+	void setNext(Node* newNext) { next = newNext; }
 	//getters
-
+	std::string getName() const { return name; }
+	std::string getArtist() const { return artist; }
+	Node* getNext() const { return next; }
 
 private:
 	std::string name;
